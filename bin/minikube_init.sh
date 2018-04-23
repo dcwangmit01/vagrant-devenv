dfa4943f1ac576c780fa5397e2b139a45d3a51f5
David C Wang <dcwangmit01@gmail.com>
2018-04-23 07:52:16 -0700

    Fixed minikube persistence issues between reboots

--- bin/minikube_init.sh
+++ bin/minikube_init.sh
@@ -1,64 +1,51 @@
 #!/bin/bash
+set -euo pipefail
 
-# Install docker
-if ! which docker; then
-    curl -fsSL https://get.docker.com/ | sh
-    usermod -aG docker root
-    usermod -aG docker vagrant
-fi
+export KUBECONFIG=${KUBECONFIG:-$HOME/.kube/minikube}
+export KUBEVERSION=${KUBEVERSION:-v1.10.0}
 
-# Install kubectl
-if ! which kubectl; then
-    curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-    chmod +x kubectl
-fi
+# sudo is default configured to drop this path
+export PATH=$PATH:/usr/local/bin
 
-# Install minikube
-if ! which minikube; then
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
-fi
-
-# Rewire minikube storage locations to ones that will survive reboots
-sudo mkdir -p /persist
-sudo mkdir -p /persist/data
-sudo mkdir -p /persist/var/lib/localkube
-sudo mkdir -p /persist/tmp/hostpath_pv
-sudo mkdir -p /persist/tmp/hostpath-provisioner
-sudo ln -sf /persist/data /
-sudo ln -sf /persist/var/lib/localkube /var/lib/
-sudo ln -sf /persist/tmp/hostpath_pv /tmp/
-sudo ln -sf /persist/tmp/hostpath-provisioner /tmp/
-
-# Set the persistent directory to be rw for all
-sudo setfacl -R -m d:u::rwx /persist
-sudo setfacl -R -m d:g::rwx /persist
-sudo setfacl -R -m d:o::rwx /persist
-sudo setfacl -R -m u::rwx /persist
-sudo setfacl -R -m g::rwx /persist
-sudo setfacl -R -m o::rwx /persist
-
-# Taken from: https://github.com/kubernetes/minikube/blob/master/README.md
-export MINIKUBE_WANTUPDATENOTIFICATION=false
-export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
+# Minikube Enviroment Variables
+#   https://github.com/kubernetes/minikube/blob/master/docs/env_vars.md
+#   Code below taken from: https://github.com/kubernetes/minikube/blob/master/README.md
+export MINIKUBE_HOME=${MINIKUBE_HOME:-$HOME/.minikube}
+export MINIKUBE_WANTUPDATENOTIFICATION=${MINIKUBE_WANTUPDATENOTIFICATION:-false}
+export MINIKUBE_WANTREPORTERRORPROMPT=${MINIKUBE_WANTREPORTERRORPROMPT:-false}
+export MINIKUBE_WANTNONEDRIVERWARNING=false
 export CHANGE_MINIKUBE_NONE_USER=true
 
-if [ ! -e $HOME/.kube ]; then
-  mkdir -p $HOME/.kube
-fi
-touch $HOME/.kube/config
 
-export KUBECONFIG=$HOME/.kube/config
+# Bind-mount these paths to a common location which is more easily backed up
+#   Tested to be unnecessary: "/var/lib/docker"
+bind_dirs=( "/data" "/var/lib/localkube" \
+    "/tmp/hostpath_pv" "/tmp/hostpath-provisioner" "/etc/kubernetes" \
+    "$MINIKUBE_HOME" )
+for bind_dir in "${bind_dirs[@]}"; do
+    sudo mkdir -p $bind_dir
+    sudo mkdir -p "/persist${bind_dir}"
+    if ! (sudo mount | grep "/persist${bind_dir}" &>/dev/null); then
+	sudo mount --bind "/persist${bind_dir}" $bind_dir
+    fi
+done
 
-sudo -E minikube start --vm-driver=none
+minikube start \
+  --vm-driver=none \
+  --kubernetes-version $KUBEVERSION \
+  --apiserver-name localhost \
+  --apiserver-ips 127.0.0.1 \
+  --loglevel 1 \
+  --v 2
 
 # this for loop waits until kubectl can access the api server that Minikube has created
+echo "Waiting for Minikube to become available"
 for i in {1..150}; do # timeout for 5 minutes
-   kubectl get po &> /dev/null
-   if [ $? -ne 1 ]; then
-      break
+  if ! kubectl get po &> /dev/null; then
+    break
   fi
   sleep 2
 done
 
 # kubectl commands are now able to interact with Minikube cluster
+echo "Minikube has successfully started"
